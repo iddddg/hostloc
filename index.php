493cde166b08b755fa5ddbd55d52cf8e042f396d
淡定定定哥 <iddddg@qq.com>
2023-07-20 18:17:36 +0800

    签到失败通知功能修复

--- index.php
+++ index.php
@@ -2,22 +2,18 @@
 // 设置默认时区
 date_default_timezone_set("PRC");
 
-// 多账号复制多个,一行一个账号密码
-// $account[num] = ['账号', '密码'];
-$account[] = ['账号1', 'password'];
-$account[] = ['账号2', 'password1'];
-// 【注意】增加/删除/修改账号等操作请删除代码目录下的account.dat(如果有的话)文件
+// ↓↓↓↓↓↓↓↓↓↓配置开始（修改配置后，如果当前目录下有account.dat文件需删除！）↓↓↓↓↓↓↓↓↓↓
 
-// 签到失败通知
-// Server酱 SCKEY 获取方法：http://sc.ftqq.com
-$ft_sckey = "******";
-// TgBot SCKEY 获取方法：Telegram关注 @onePushBot 发送/start即可获取
-$tg_sckey = "******";
+// 多账号复制多个，一行一个账号密码
+$account[] = ['账号1', '密码1'];
+$account[] = ['账号2', '密码2'];
+// 签到失败通知KEY 获取方法：Telegram关注 @onePushBot 发送/key即可获取
+$tg_push_key = '';
 
-// 留空勿填
-$cookie = "";
+// ↑↑↑↑↑↑↑↑↑↑配置结束（修改配置后，如果当前目录下有account.dat文件需删除！）↑↑↑↑↑↑↑↑↑↑
 
 // Go
+$cookie = "";
 $need_brush = need_brush($account);
 brush($need_brush);
 
@@ -27,7 +23,7 @@ function need_brush($account)
     $file = 'account.dat';
     if (file_exists($file)) {
         $dat = json_decode(file_get_contents($file), 1);
-    }else{ 
+    } else {
         foreach ($account as $key => $value) {
             $dat[$key] = [
                 'username' => $value[0],
@@ -38,7 +34,7 @@ function need_brush($account)
         }
         file_put_contents($file, json_encode($dat));
     }
-    
+
     foreach ($dat as $key => $value) {
         if ($value['status'] == 'suc' && $value['date'] == date('Y-m-d')) {
             unset($dat[$key]);
@@ -56,29 +52,26 @@ function brush($need_brush)
         $data = login($value['username'], $value['password']);
         if ($data['username'] == $value['username']) {
             echo "登录成功（{$value['username']}）\n";
+            echo "初始信息（用户组:{$data['group']},金钱:{$data['money']},威望:{$data['prestige']},积分:{$data['point']}）\n";
+            echo "刷分中 ";
+            for ($i = 31180; $i < 31210; $i++) {
+                $html = http_get(str_replace('*', $i, 'https://hostloc.com/space-uid-*.html'));
+                echo $i == 31209 ? "+ 完成\n" : "+";
+                sleep(rand(5, 10));
+            }
+            $data = get_info();
+            echo "结束信息（用户组:{$data['group']},金钱:{$data['money']},威望:{$data['prestige']},积分:{$data['point']}）\n";
+            echo date("Y-m-d H:i:s\n");
+            echo "----------------------------------------------------------\n";
+            success($key);
+            unset($need_brush[$key]);
         } else {
             echo "登录失败（{$value['username']}）\n";
             echo date("Y-m-d H:i:s\n");
             echo "----------------------------------------------------------\n";
-            continue;
         }
-        echo "初始信息（用户组:{$data['group']},金钱:{$data['money']},威望:{$data['prestige']},积分:{$data['point']}）\n";
-        echo "刷分中 ";
-        for ($i = 31180; $i < 31210; $i++) {
-            $html = http_get(str_replace('*', $i, 'https://hostloc.com/space-uid-*.html'));
-            echo $i == 31209 ? "+ 完成\n" : "+";
-            sleep(rand(5, 10));
-        }
-        $data = get_info();
-        echo "结束信息（用户组:{$data['group']},金钱:{$data['money']},威望:{$data['prestige']},积分:{$data['point']}）\n";
-        echo date("Y-m-d H:i:s\n");
-        echo "----------------------------------------------------------\n";
-
-        success($key);
-        unset($need_brush[$key]);
-        sleep(rand(1, 60));
+        sleep(rand(5, 30));
     }
-
     notice($need_brush);
 }
 
@@ -95,7 +88,6 @@ function login($username, $password)
         'cookietime' => 2592000
     );
     $login = http_post('https://hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1', $loginData, $cookie);
-
     // preg_match("/cookie=\"(\w*?)\=(\w*)/", $login, $cookie);
     // preg_match("/href=\"(.*?)\"/", $login, $url);
 
@@ -103,10 +95,7 @@ function login($username, $password)
     //     $cookie = "{$cookie[1]}={$cookie[2]};";
     //     http_post($url[1], $loginData, $cookie);
     // }
-
-    $data = get_info();
-
-    return $data;
+    return get_info();
 }
 
 // 获取个人信息
@@ -172,9 +161,9 @@ function http_get($url)
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_TIMEOUT, 600);
-    $result = curl_exec($ch);
+    $response = curl_exec($ch);
     curl_close($ch);
-    return $result;
+    return $response;
 }
 
 // POST请求
@@ -197,9 +186,9 @@ function http_post($url, $data)
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_TIMEOUT, 600);
-    $result = curl_exec($ch);
+    $response = curl_exec($ch);
     curl_close($ch);
-    return $result;
+    return $response;
 }
 
 // 成功更新状态和日期
@@ -215,29 +204,36 @@ function success($key)
 // 通知
 function notice($err_account)
 {
-    global $ft_sckey;
-    global $tg_sckey;
-
+    global $tg_push_key;
     if (!empty($err_account) && date('H') == 21) {
         $username = array_column($err_account, 'username');
         $title = 'Hostloc签到失败';
         $content = '您的账号（' . implode('，', $username) . '），签到失败';
-        
-        // Server酱 通知
-        if ($ft_sckey) {
-            http_post("https://sc.ftqq.com/" . $ft_sckey . ".send", [
-                "text" => $title,
-                "desp" => $content
-            ]);
-        }
-
+        $data = array(
+            "key" => $tg_push_key,
+            "text" => $title . "\n" . $content
+        );
         // Telegram 通知
-        if ($tg_sckey) {
-            http_post("https://telegram.ddddg.cn/bot.php", [
-                "method" => "send",
-                "content" => $title . "\n" .$content,
-                "sckey" => $tg_sckey
-            ]);
+        if ($tg_push_key) {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://tg-bot.t04.net/push',
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => false,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 600,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => json_encode($data),
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json'
+                ),
+            ));
+            curl_exec($curl);
+            curl_close($curl);
         }
     }
 }
