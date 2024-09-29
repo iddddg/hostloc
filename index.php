b75bbf9cbaa0e01974d49295a4b762426ac8eea4
iddddg <iddddg@qq.com>
2024-09-29 15:25:06 +0800

    :bug:修复开启CC防护无法登录的问题

--- index.php
+++ index.php
@@ -73,19 +73,76 @@ function brush()
 function login($username, $password)
 {
     global $cookie;
+    $cookie = '';
     $loginData = array(
-        "username" => $username,
-        "password" => $password,
-        "fastloginfield" => "username",
-        "quickforward" => "yes",
-        "handlekey" => "ls",
-        'cookietime' => 2592000
+        'fastloginfield' => 'username',
+        'username' => $username,
+        'cookietime' => 2592000,
+        'password' => $password,
+        // 'formhash' => '751cf73e',
+        'quickforward' => 'yes',
+        'handlekey' => 'ls'
     );
-    $response = http_post('https://hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1', $loginData);
+    $cc_cookie = get_cc_cookie();
+    $response = http_post('https://hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1', $loginData, $cc_cookie);
     // 获取Cookie
     preg_match_all('/set-cookie: (.*?);/i', $response, $matches);
-    $cookie = implode(';', $matches[1]);
+    if (count($cc_cookie) > 0) {
+        $cookie = http_build_query($cc_cookie, '', ';') . ';';
+    }
+    $cookie .= implode(';', $matches[1]);
     return get_info();
+}
+
+/**
+ * 获取CC防护的Cookie参数
+ * @return array
+ */
+function get_cc_cookie()
+{
+    $cc_cookie = [];
+    // 检测防CC机制的参数
+    $data = check_cc();
+    // 开启了防CC机制
+    if (count($data) === 4) {
+        // 密钥
+        $a = hex2bin($data['a']);
+        // 初始化向量 (IV)
+        $b = hex2bin($data['b']);
+        // 加密后的数据
+        $c = hex2bin($data['c']);
+        // AES CBC 解密
+        $value = openssl_decrypt($c, 'AES-128-CBC', $a, OPENSSL_NO_PADDING, $b);
+
+        $cc_cookie[$data['cookie']] = bin2hex($value);
+    }
+
+    return $cc_cookie;
+}
+
+
+/**
+ * 检查是否开启CC防护
+ * @return array
+ */
+function check_cc()
+{
+    $data = [];
+    $html = file_get_contents('https://hostloc.com/forum.php');
+
+    preg_match_all("/toNumbers\(\"(.*?)\"\)/", $html, $matches);
+    if (isset($matches[1]) && count($matches[1]) === 3) {
+        $data['a'] = $matches[1][0];
+        $data['b'] = $matches[1][1];
+        $data['c'] = $matches[1][2];
+    }
+
+    preg_match("/cookie=\"(.*?)=\"/", $html, $matches);
+    if (isset($matches[1])) {
+        $data['cookie'] = $matches[1];
+    }
+
+    return $data;
 }
 
 /**
@@ -96,37 +153,37 @@ function get_info()
 {
     $data = [];
     $html = http_get('https://hostloc.com/home.php?mod=spacecp&ac=credit');
-    preg_match('/<a.*?title="访问我的空间">(.*)<\/a>/', $html, $preg);
-    if (isset($preg[1])) {
-        $data['username'] = $preg[1];
+    preg_match('/<a.*?title="访问我的空间">(.*)<\/a>/', $html, $matches);
+    if (isset($matches[1])) {
+        $data['username'] = $matches[1];
     } else {
         $data['username'] = '';
     }
 
-    preg_match("/>用户组: (.*?)<\/a>/", $html, $preg);
-    if (isset($preg[1])) {
-        $data['group'] = $preg[1];
+    preg_match("/>用户组: (.*?)<\/a>/", $html, $matches);
+    if (isset($matches[1])) {
+        $data['group'] = $matches[1];
     } else {
         $data['group'] = '?';
     }
 
-    preg_match("/金钱: <\/em>(\d+)/", $html, $preg);
-    if (isset($preg[1])) {
-        $data['money'] = $preg[1];
+    preg_match("/金钱: <\/em>(\d+)/", $html, $matches);
+    if (isset($matches[1])) {
+        $data['money'] = $matches[1];
     } else {
         $data['money'] = '?';
     }
 
-    preg_match("/威望: <\/em>(\d+)/", $html, $preg);
-    if (isset($preg[1])) {
-        $data['prestige'] = $preg[1];
+    preg_match("/威望: <\/em>(\d+)/", $html, $matches);
+    if (isset($matches[1])) {
+        $data['prestige'] = $matches[1];
     } else {
         $data['prestige'] = '?';
     }
 
-    preg_match("/积分: (\d+)<\/a>/", $html, $preg);
-    if (isset($preg[1])) {
-        $data['point'] = $preg[1];
+    preg_match("/积分: (\d+)<\/a>/", $html, $matches);
+    if (isset($matches[1])) {
+        $data['point'] = $matches[1];
     } else {
         $data['point'] = '?';
     }
@@ -166,13 +223,14 @@ function http_get($url)
  * @param $data
  * @return bool|string
  */
-function http_post($url, $data)
+function http_post($url, $data, $cookie = [])
 {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_COOKIE, http_build_query($cookie, '', '; '));
     curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36');
     curl_setopt($ch, CURLOPT_REFERER, 'https://hostloc.com/');
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
